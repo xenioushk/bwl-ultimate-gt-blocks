bc8b89b582683d3ffa2e6fe69678a48c35a75566
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-12-28 23:32:24 +0200

    Updated button block.json file to include the editor and frontend styles. Added mixins to SCSS file.

--- src/button/edit.js
+++ src/button/edit.js
@@ -5,7 +5,6 @@ import {
 	BlockControls,
 } from '@wordpress/block-editor';
 import { ToolbarGroup, ToolbarButton } from '@wordpress/components';
-import './editor.scss';
 export default function Edit({ attributes, setAttributes }) {
 	const blockProps = useBlockProps();
 
